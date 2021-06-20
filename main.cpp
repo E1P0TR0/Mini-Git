6946f7998c65a748c0620310a984423aa943011d
E1P0TR0 <rotogui143@gmail.com>
2021-06-19 20:55:32 -0500

    Se implementó el registro y logeo correctamente

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include "controller.hpp"
 
+#include "listaEnlazada.hpp"
+
 int main()
 {
     Controller* objController = new Controller();
+
+
+    // // Prueba lista
+    // LinkedList<User*>* list = new LinkedList<User*>();
+    // User* u1 = new User("a", "a", "a");
+    // list->addFinal(u1);
+    // User* u2 = new User("b", "b", "b");
+    // list->addFinal(u2);
+    // User* u3 = new User("c", "c", "c");
+    // list->addFinal(u3);
+
+    // list->printList();
 
     system("pause>0");
     return 0;
 }
 
+// DIAGRAMA DE CLASES (ENTIDADES)
+// DEFINIR REQUISITOS
+// DISEÑO DE INTERFAZ DE USUARIO
+// TIPOS DE DE DATOS ABSTRACTOS
+// SELECCIÓN DE ESTRUCTURAS DE DATOS
+// IMPLEMENTACIÓN DE LAS ESTRUCTURAS
+// DIAGRAMA DE CLASES (COMPONENTES)
+// IMPLEMENTACIÓN DE LAS CLASES ENTIDAD
+// PROTOTIPO APLICACIÓN
